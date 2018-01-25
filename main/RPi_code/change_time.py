15cd3d0d3782728bc8998cfbe017d0b857ae4bbe
Kevin Cabrera <kcabrer16@gmail.com>
2018-01-24 21:57:43 -0700

    Update main.ino and update RPi scripts Jan 24: 	- fix RPi script to change the hourOn time 	- added note to fix the Arduino voltage being read incorrectly 	- fix temp sensor code to use the correct voltage reference

--- main/RPi_code/change_time.py
+++ main/RPi_code/change_time.py
@@ -1,5 +1,11 @@
 import serial
+import sys
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-ser.write("t110")
+time = int(sys.argv[1]);
+
+if time < 10:
+	ser.write('t10%d' % time);
+else:
+	ser.write('t1%d' % time);
 ser.close()
--- main/TemperatureSensor.cpp
+++ main/TemperatureSensor.cpp
@@ -20,9 +20,22 @@ bool TemperatureSensor::changePinNumber(int pin)
 
 double TemperatureSensor::collectSingleReading()
 {
-	singleReading = analogRead(tempPin);
-	singleReading = convertReadingToUsable(singleReading);
-	return singleReading;
+	// singleReading = analogRead(tempPin);
+	// singleReading = convertReadingToUsable(singleReading);
+	double tempF;
+
+	int reading = analogRead(4);
+	Serial.println(reading);
+
+	double voltage = reading * 4.53; // 4.53 is the voltage being supplied to the sensor
+	voltage /= 1024; // 1024 is the 2^10 which is the precision of the ADC using 10 bits
+	Serial.print(voltage);
+	Serial.println(" V");
+
+	double tempC = (voltage - 0.5) * 100; // 25C is 0.75 V with an offset of 0.5 V
+	tempF = (tempC * 9.0/5.0) + 32;
+
+	return tempF;
 }
 
 double TemperatureSensor::collectAvgReading()
@@ -39,12 +52,20 @@ double TemperatureSensor::collectAvgReading()
 // Converts the analog reading from the temp sensor into degrees Fahrenheit
 double TemperatureSensor::convertReadingToUsable(double reading)
 {
-	double voltage = reading * 5;
-	voltage /= 1024.0;
+	// double voltage = reading * 5;
+	// voltage /= 1024.0;
 
-	double tempC = (voltage - 0.5) * 100;
+	// double tempC = (voltage - 0.5) * 100;
+	// double tempF = (tempC * 9.0 / 5.0) + 32;
+
+	double tempC = reading * 3.3;
+	tempC /= 1024;
+	Serial.print(" - ");
+	Serial.print(tempC); Serial.println(" volts");
+	tempC = (tempC - 0.5) * 100;
+
 	double tempF = (tempC * 9.0 / 5.0) + 32;
 
-	return tempF;
+	return tempC;
 }
 
